f3ab51ca2c8c3a7ad64c5cf488565bd490cc21be
Guest User <student@epicodus.com>
2015-09-22 16:11:52 -0700

    show movie links

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
+var movies = [];
+var tickets = [];
+
 function Movie(name, showTimes){
-  //this.movies = [];
   this.name = name;
   this.showTimes = showTimes;
-  // this.movies.push(this);
+  movies.push(this);
 }
 
-var neverendingStory = new Movie("Neverending Story", ["10:30 AM", "1:15 PM", "3:00 PM", "7:30 PM"]);
-var rockyHorror = new Movie("Rocky Horror Picture Show", ["11:30 AM", "3:15 PM", "8:00 PM", "12 AM"]);
-var theMummy = new Movie("The Mummy", ["9:30 AM", "2:45 PM", "5:00 PM", "6:00 PM"]);
+Movie.prototype.nameAsId = function() {
+  return this.name.split(" ").join("-");
+};
 
 function Ticket(ticketType, price) {
   this.ticketType = ticketType;
   this.price = price;
+  tickets.push(this);
 }
 
 var adult = new Ticket("Adult", "$10.00");
@@ -19,52 +22,52 @@ var matinee = new Ticket("Matinee", "$8.50");
 var student = new Ticket("Student", "$7.00");
 var senior = new Ticket("Senior", "$9.00");
 
-var showMovies = function() {
-  return neverendingStory;
-  return rockyHorror;
-  return theMummy;
-  showMovies();
-}
 
+var movie1 = new Movie("Neverending Story", ["10:30 AM", "1:15 PM", "3:00 PM", "7:30 PM"]);
+var movie2 = new Movie("Rocky Horror Picture Show", ["11:30 AM", "3:15 PM", "8:00 PM", "12 AM"]);
+var movie3 = new Movie("The Mummy", ["9:30 AM", " 2:45 PM", " 5:00 PM", " 6:00 PM"]);
 
+$(document).ready(function() {
+  movies.forEach(function(movie){
+    $("#show-movie").prepend('<h4 id="' + movie.nameAsId() + '" class="movie">' + movie.name +'</h4>')
+    $("#" + movie.nameAsId()).click(function(){
+      alert(movie.showTimes[0]);
+    });
+  });
 
-// $(document).ready(function() {
-//   $("#add-address").click(function() {
-//
-//   });
+  // $("#show-movie h4").text(newMovie2.name + ", Showtimes: " + newMovie2.showTimes);
+  //
+  //
+  // $("#show-movie h4").text(newMovie3.name + ", Showtimes: " + newMovie3.showTimes);
+});
+
+  // $("#movie2").click(function() {
+  //   .show();
+  // });
+  //
+  // $("#movie3").click(function() {
+  //   .show();
+  // });
 //
 //   $("form#new-contact").submit(function(event) {
 //     event.preventDefault();
 //
 //     var inputtedFirstName = $("input#new-first-name").val();
-//     var inputtedLastName = $("input#new-last-name").val();
 //
 //     var newContact = new Contact(inputtedFirstName, inputtedLastName);
-//
-//     $(".new-address").each(function() {
-//       var inputtedLocale = $(this).find("input.new-locale").val();
-//       var inputtedStreet = $(this).find("input.new-street").val();
-//       var inputtedCity = $(this).find("input.new-city").val();
-//       var inputtedState = $(this).find("input.new-state").val();
 //
 //       var newAddress = new Address(inputtedLocale, inputtedStreet, inputtedCity, inputtedState)
 //       newContact.addresses.push(newAddress);
 //     });
-//
-//
-//
-//     $(".contact").last().click(function() {
-//       $("#show-contact").show();
-//       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-//       $(".first-name").text(newContact.firstName);
-//       $(".last-name").text(newContact.lastName);
-//
-//       $("ul#addresses").text("");
-//       newContact.addresses.forEach(function(address) {
-//         //if  $("li").not( ": , ,").show();
-//         $("ul#addresses").append("<li>" + address.locale + ": " + address.street + ", " + address.city + ", " + address.state + "</li>");
-//
-//       });
+// $("ul#showTimes").append("<li><span class='showTimes'>" + movie1 + "</span></li>");
+//     $(".movie").click(function() {
+//       $(".showTimes").show();
+//       $(".showTimes movie1 h2").text(movie1.name);
+//       // $(".showTimes .movie1 p").text(movie1.showTimes);
+//       $("ul").append("<li>" + movie1.showTimes() + "</li>");
+//       // $("#show-movie .movie2").text(movie2.showTimes);
+//       // $("#show-movie .movie3").text(movie3.showTimes);
+//     });
 //     });
 //   });
 // });
