ddd3d96b99514b0cc783b36e24d23959fe7db72a
Guest User <student@epicodus.com>
2015-09-22 14:24:33 -0700

    creates movie and ticket objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,101 +1,70 @@
-function Movie(name,showTime){
+function Movie(name, showTimes){
+  //this.movies = [];
   this.name = name;
-  this.showTime = [];
+  this.showTimes = showTimes;
+  // this.movies.push(this);
 }
 
-// Contact.prototype.fullName = function() {
-//   return this.firstName + " " + this.lastName;
-// }
+var neverendingStory = new Movie("Neverending Story", ["10:30 AM", "1:15 PM", "3:00 PM", "7:30 PM"]);
+var rockyHorror = new Movie("Rocky Horror Picture Show", ["11:30 AM", "3:15 PM", "8:00 PM", "12 AM"]);
+var theMummy = new Movie("The Mummy", ["9:30 AM", "2:45 PM", "5:00 PM", "6:00 PM"]);
 
-function Ticket(price, ticketType) {
-  this.ticketType = [];
-  this.price = [];
+function Ticket(ticketType, price) {
+  this.ticketType = ticketType;
+  this.price = price;
 }
 
-Address.prototype.fullAddress = function() {
-  return this.locale + ": " + this.street + ", " + this.city + ", " + this.state;
+var adult = new Ticket("Adult", "$10.00");
+var matinee = new Ticket("Matinee", "$8.50");
+var student = new Ticket("Student", "$7.00");
+var senior = new Ticket("Senior", "$9.00");
+
+var showMovies = function() {
+  return neverendingStory;
+  return rockyHorror;
+  return theMummy;
+  showMovies();
 }
 
-function resetFields() {
-  $("input#new-first-name").val("");
-  $("input#new-last-name").val("");
-  $("input.new-locale").val("");
-  $("input.new-street").val("");
-  $("input.new-city").val("");
-  $("input.new-state").val("");
-}
-
-$(document).ready(function() {
-  $("#add-address").click(function() {
-    $("#new-addresses").append('<div class="new-address">' +
-    '<div class="form-group">' +
-    '<label for="new-locale">Home or Buisness:</label>' +
-    '<input type="text" class="form-control new-locale">' +
-    '</div>' +
-    '<div class="form-group">' +
-    '<label for="new-street">Street</label>' +
-    '<input type="text" class="form-control new-street">' +
-    '</div>' +
-    '<div class="form-group">' +
-    '<label for="new-city">City</label>' +
-    '<input type="text" class="form-control new-city">' +
-    '</div>' +
-    '<div class="form-group">' +
-    '<label for="new-state">State</label>' +
-    '<input type="text" class="form-control new-state">' +
-    '</div>' +
-    '</div>');
-  });
-
-  $("form#new-contact").submit(function(event) {
-    event.preventDefault();
-
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-
-    var newContact = new Contact(inputtedFirstName, inputtedLastName);
-
-    $(".new-address").each(function() {
-      var inputtedLocale = $(this).find("input.new-locale").val();
-      var inputtedStreet = $(this).find("input.new-street").val();
-      var inputtedCity = $(this).find("input.new-city").val();
-      var inputtedState = $(this).find("input.new-state").val();
-
-      var newAddress = new Address(inputtedLocale, inputtedStreet, inputtedCity, inputtedState)
-      newContact.addresses.push(newAddress);
-    });
 
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-
-      $("#show-contact h2").text(newContact.fullName());
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-
-      $("ul#addresses").text("");
-
-      newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
-      });
-    });
-
-    resetFields();
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-
-      $("ul#addresses").text("");
-      newContact.addresses.forEach(function(address) {
-        //if  $("li").not( ": , ,").show();
-        $("ul#addresses").append("<li>" + address.locale + ": " + address.street + ", " + address.city + ", " + address.state + "</li>");
-
-      });
-    });
-  });
-});
+// $(document).ready(function() {
+//   $("#add-address").click(function() {
+//
+//   });
+//
+//   $("form#new-contact").submit(function(event) {
+//     event.preventDefault();
+//
+//     var inputtedFirstName = $("input#new-first-name").val();
+//     var inputtedLastName = $("input#new-last-name").val();
+//
+//     var newContact = new Contact(inputtedFirstName, inputtedLastName);
+//
+//     $(".new-address").each(function() {
+//       var inputtedLocale = $(this).find("input.new-locale").val();
+//       var inputtedStreet = $(this).find("input.new-street").val();
+//       var inputtedCity = $(this).find("input.new-city").val();
+//       var inputtedState = $(this).find("input.new-state").val();
+//
+//       var newAddress = new Address(inputtedLocale, inputtedStreet, inputtedCity, inputtedState)
+//       newContact.addresses.push(newAddress);
+//     });
+//
+//
+//
+//     $(".contact").last().click(function() {
+//       $("#show-contact").show();
+//       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+//       $(".first-name").text(newContact.firstName);
+//       $(".last-name").text(newContact.lastName);
+//
+//       $("ul#addresses").text("");
+//       newContact.addresses.forEach(function(address) {
+//         //if  $("li").not( ": , ,").show();
+//         $("ul#addresses").append("<li>" + address.locale + ": " + address.street + ", " + address.city + ", " + address.state + "</li>");
+//
+//       });
+//     });
+//   });
+// });
--- spec/specs.js
+++ spec/specs.js
@@ -1,17 +1,21 @@
 describe('Movie', function() {
   it("creates a movie with a name and times it is showing", function() {
-    var testMovie = new Movie("Neverending Story", "10:15 AM");
-    expect(testMovie.name).to.equal("Neverending Story");
-    expect(testMovie.showTime).to.eql([]);
+    var neverendingStory = new Movie("Neverending Story", ["10:30 AM"]);
+    expect(neverendingStory.name).to.equal("Neverending Story");
+    expect(neverendingStory.showTimes).to.eql(["10:30 AM"]);
+  });
 
+  it("assigns movie a name and showtimes", function() {
+    var rockyHorror = new Movie("Rocky Horror Picture Show", ["11:30 AM"]);
+    expect(rockyHorror.name).to.equal("Rocky Horror Picture Show");
+    expect(rockyHorror.showTimes).to.eql(["11:30 AM"]);
   });
 });
 
 describe('Ticket', function() {
   it("creates a ticket with a price and the type of ticket it is", function() {
-    var testTicket = new Ticket("$9.50", "Student");
-    expect(testTicket.price).to.eql([]);
-    expect(testTicket.ticketType).to.eql([]);
-
+    var testTicket = new Ticket("Student", "$7.00");
+    expect(testTicket.ticketType).to.eql("Student");
+    expect(testTicket.price).to.eql("$7.00");
   });
 });
