6ebef68405029add03ffe9c721f7dc131c6ddb3f
Guest User <student@epicodus.com>
2015-09-22 12:22:41 -0700

    pass first two specs

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,17 @@
-describe('functionName', function() {
-  it("describe behavior", function() {
-    expect(functionName(parameter)).to.equal(result);
+describe('Movie', function() {
+  it("creates a movie with a name and times it is showing", function() {
+    var testMovie = new Movie("Neverending Story", "10:15 AM");
+    expect(testMovie.name).to.equal("Neverending Story");
+    expect(testMovie.showTime).to.eql([]);
+
+  });
+});
+
+describe('Ticket', function() {
+  it("creates a ticket with a price and the type of ticket it is", function() {
+    var testTicket = new Ticket("$9.50", "Student");
+    expect(testTicket.price).to.eql([]);
+    expect(testTicket.ticketType).to.eql([]);
+
   });
 });
